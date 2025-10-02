87a8ceaf42ab4ef9efdf8a120dd77fdfab5e4d80
Gosia2024 <lm.sprachen@gmail.com>
2025-10-02 10:50:04 +0200

    chore: add remaining untracked files (configs, pages, assets)

---
+++ docusaurus.config.ts
@@ -0,0 +1,187 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import {config as dotenvconfig}  from "dotenv";
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenvconfig();
+
+/* TODO: change to read configuration from environment */
+const blogEnabled = Boolean(process.env.BLOG_ENABLED === 'true')
+
+const config: Config = {
+  title: 'DSO Live Demo Docs',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: process.env.DEPLOYMENT_URL ?? "https://gosia2024.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/blog-dev/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Gosia2024', // Usually your GitHub org/user name.
+  projectName: 'blog-dev', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+  defaultLocale: 'en',          
+  locales: ['pl', 'de', 'en'],  
+  localeConfigs: {
+    pl: { label: 'PL' },
+    de: { label: 'DE' },
+    en: { label: 'EN' },
+  },
+},
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/spmse/dev-blog-template',
+        },
+        blog: blogEnabled ? 
+          {
+            showReadingTime: true,
+            feedOptions: {
+              type: ['rss', 'atom'],
+              xslt: true,
+            },
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl:
+              'https://github.com/spmse/dev-blog-template',
+            // Useful options to enforce blogging best practices
+            onInlineTags: 'warn',
+            onInlineAuthors: 'warn',
+            onUntruncatedBlogPosts: 'warn',
+          }
+          : false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/spmse/dev-blog-template',
+          label: 'Github',
+          position: 'right',
+        },
+        { type: 'localeDropdown', position: 'right' },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      logo: {
+        src: 'img/logo.png',  // <--- ścieżka do pliku w /static/img
+    href: '/',             // (opcjonalne) link po kliknięciu
+    width: 90,              // (opcjonalnie) rozmiar
+    height: 90,
+    className:'spinReverse'
+      },
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/guides/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+          label: 'My Portfolio',                     // 👈 Twój nowy link
+          href: 'http://malgorzata-lingenfelder.de',               // 👈 adres Twojej strony na FTP
+        },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Gosia. Built with Docusaurus and ❤️.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['powershell', 'hcl'],
+      magicComments: [
+        // Remember to extend the default highlight class name as well!
+        {
+          className: 'theme-code-block-highlighted-line',
+          line: 'highlight-next-line',
+          block: {start: 'highlight-start', end: 'highlight-end'},
+        },
+        {
+          className: 'code-block-error-line',
+          line: 'This will error',
+        },
+      ],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+
+if (blogEnabled) {
+  (config.themeConfig.navbar as any).items.push({to: '/blog', label: 'Blog', position: 'left'});
+  (config.themeConfig.footer as any).links[2].items.push({to: '/blog', label: 'Blog'});
+}
+
+export default config;
